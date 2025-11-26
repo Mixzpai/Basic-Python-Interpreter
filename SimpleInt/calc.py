1463885bf29ec3390f6d82628a781cf73a19f60b
Mikka <mikkajallen07@outlook.com>
2025-11-26 21:03:46 +0800

    Slight refactoring
    Made functions more readable, and organized the overall structure to >>|constants|class1|class 2|class2 helpers| class2 main execution functions|

--- SimpleInt/calc.py
+++ SimpleInt/calc.py
@@ -1,3 +1,5 @@
+#======================================================================================#
+#======================================================================================#
 # Token types
 INTEGER, REAL, STRING = 'INTEGER', 'REAL', 'STRING'
 # Operators and delimiters
@@ -14,7 +16,8 @@ LB, RB = 'LB', 'RB'
 # Assignment (reserved for later)
 ASSIGN = 'ASSIGN'
 
-
+#======================================================================================#
+#======================================================================================#
 class Token(object):
     def __init__(self, type, value):
         self.type = type
@@ -26,146 +29,37 @@ class Token(object):
     def __repr__(self):
         return self.__str__()
 
-
+#======================================================================================#
+#======================================================================================#
 class Interpreter(object):
     def __init__(self, text):
         self.text = text
         self.pos = 0
         self.current_token = None
-
+        self.current_char = self.text[self.pos] if self.text else None
+    #==================================================================#
+    #==================================================================#
     def error(self):
         raise Exception('Error parsing input')
 
-    def get_next_token(self):
-        text = self.text
+    def skip_whitespace(self, text):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance_pos()
 
-        # Skip whitespace
-        while self.pos < len(text) and text[self.pos].isspace():
-            self.pos += 1
+    def peek(self, text):
+        next_pos = self.pos + 1
+        if next_pos > len(text) - 1:
+            return None
+        return text[next_pos]
 
-        if self.pos > len(text) - 1:
-            return Token(EOF, None)
-
-        current_char = text[self.pos]
-
-        def peek():
-            next_pos = self.pos + 1
-            if next_pos > len(text) - 1:
-                return None
-            return text[next_pos]
-
-        # Multi-digit integer
-        if current_char.isdigit():
-            tk = ''
-            while self.pos < len(self.text) and self.text[self.pos].isdigit():
-                tk += self.text[self.pos]
-                self.pos += 1
-            return Token(INTEGER, int(tk))
-
-        # Operators
-        if current_char == '+':
-            self.pos += 1
-            return Token(PLUS, '+')
-
-        if current_char == '-':
-            self.pos += 1
-            return Token(MINUS, '-')
-
-        if current_char == '*':
-            self.pos += 1
-            return Token(MUL, '*')
-
-        if current_char == '%':
-            self.pos += 1
-            return Token(MOD, '%')
-
-        if current_char == '/':
-            self.pos += 1
-            return Token(DIV, '/')
-
-        if current_char == '^':
-            self.pos += 1
-            return Token(POW, '^')
-
-        # Special functions
-        if current_char == 's':
-            # Match exactly 4 chars: 'sqrt'
-            if self.text[self.pos:self.pos + 4] == 'sqrt':
-                token = Token(SQRT, 'sqrt')
-                self.pos += 4
-                return token
-            else:
-                self.error()
-
-        if current_char == 'l':
-            if self.text[self.pos:self.pos + 4] == 'log2':
-                self.pos += 4
-                return Token(LOG, 'log2')
-            elif self.text[self.pos:self.pos + 5] == 'log10':
-                self.pos += 5
-                return Token(LOG, 'log10')
-            else:
-                self.error()
-
-        # Parentheses
-        if current_char == '(':
-            self.pos += 1
-            return Token(LP, '(')
-
-        if current_char == ')':
-            self.pos += 1
-            return Token(RP, ')')
-
-        # Square brackets
-        if current_char == '[':
-            self.pos += 1
-            return Token(LB, '[')
-
-        if current_char == ']':
-            self.pos += 1
-            return Token(RB, ']')
-
-        # Comparisons
-        if current_char == '>':
-            if peek() == '=':
-                self.pos += 2
-                return Token(GE, '>=')
-            else:
-                self.pos += 1
-                return Token(GT, '>')
-
-        if current_char == '<':
-            if peek() == '=':
-                self.pos += 2
-                return Token(LE, '<=')
-            else:
-                self.pos += 1
-                return Token(LT, '<')
-
-        if current_char == '=':
-            if peek() == '=':
-                self.pos += 2
-                return Token(EQ, '==')
-            else:
-                self.error()
-
-        if current_char == '!':
-            if peek() == '=':
-                self.pos += 2
-                return Token(NE, '!=')
-            else:
-                self.error()
-
-        self.error()
-
-    def eat(self, token_type):
-        if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
+    def advance_pos(self, x=1):
+        self.pos += x
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
         else:
-            self.error()
-
-    # ln helper for log2/log10
-    def _ln(self, x, term = 20):
+            self.current_char = self.text[self.pos]
+   
+    def _ln(self, x, term=20):
         if x <= 0:
             raise ValueError("Domain error: logarithm undefined for x <= 0")
         t = (x - 1) / (x + 1)
@@ -177,50 +71,139 @@ class Interpreter(object):
             t_pow = t_pow * t * t
             k += 2
         return 2 * result
+    
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+    #==================================================================#
+    #==================================================================#    
+    def get_next_token(self):
+        text = self.text
 
+        self.skip_whitespace(text)
+
+        if self.pos > len(text) - 1:
+            return Token(EOF, None)
+
+        if self.current_char.isdigit():
+            tk = ''
+            while self.pos < len(self.text) and self.current_char.isdigit():
+                tk += self.text[self.pos]
+                self.advance_pos()
+            return Token(INTEGER, int(tk))
+
+        match self.current_char:
+            case '+':
+                self.advance_pos()
+                return Token(PLUS, '+')
+            case '-':
+                self.advance_pos()
+                return Token(MINUS, '-')
+            case '*':
+                self.advance_pos()
+                return Token(MUL, '*')
+            case '%':
+                self.advance_pos()
+                return Token(MOD, '%')
+            case '/':
+                self.advance_pos()
+                return Token(DIV, '/')
+            case '^':
+                self.advance_pos()
+                return Token(POW, '^')
+            case '(':
+                self.advance_pos()
+                return Token(LP, '(')
+            case ')':
+                self.advance_pos()
+                return Token(RP, ')')
+            case '[':
+                self.advance_pos()
+                return Token(LB, '[')
+            case ']':
+                self.advance_pos()
+                return Token(RB, ']')
+            case 's':
+                if self.text[self.pos:self.pos + 4] == 'sqrt':
+                    token = Token(SQRT, 'sqrt')
+                    self.advance_pos(4)
+                    return token
+                self.error()
+            case 'l':
+                if self.text[self.pos:self.pos + 4] == 'log2':
+                    self.advance_pos(4)
+                    return Token(LOG, 'log2')
+                if self.text[self.pos:self.pos + 5] == 'log10':
+                    self.advance_pos(5)
+                    return Token(LOG, 'log10')
+                self.error()
+            case '>':
+                if self.peek(text) == '=':
+                    self.advance_pos(2)
+                    return Token(GE, '>=')
+                self.advance_pos()
+                return Token(GT, '>')
+            case '<':
+                if self.peek(text) == '=':
+                    self.advance_pos(2)
+                    return Token(LE, '<=')
+                self.advance_pos()
+                return Token(LT, '<')
+            case '=':
+                if self.peek(text) == '=':
+                    self.advance_pos(2)
+                    return Token(EQ, '==')
+                self.error()
+            case '!':
+                if self.peek(text) == '=':
+                    self.advance_pos(2)
+                    return Token(NE, '!=')
+                self.error()
+            case _:
+                self.error()
+    #==================================================================#
+    #==================================================================#
     def factor(self):
         token = self.current_token
 
-        if token.type == PLUS:
-            self.eat(PLUS)
-            return +self.factor()
-
-        if token.type == MINUS:
-            self.eat(MINUS)
-            return -self.factor()
-
-        if token.type == INTEGER:
-            self.eat(INTEGER)
-            return token.value
-
-        if token.type == SQRT:
-            self.eat(SQRT)
-            self.eat(LP)
-            inner = self.expr()
-            self.eat(RP)
-            return inner ** 0.5
-
-        if token.type == LOG:
-            op = token.value
-            self.eat(LOG)
-            self.eat(LP)
-            inner = self.expr()
-            self.eat(RP)
-            if op == 'log2':
-                return self._ln(inner) / self._ln(2)
-            elif op == 'log10':
-                return self._ln(inner) / self._ln(10)
-            else:
+        match token.type:
+            case 'PLUS':
+                self.eat(PLUS)
+                return +self.factor()
+            case 'MINUS':
+                self.eat(MINUS)
+                return -self.factor()
+            case 'INTEGER':
+                self.eat(INTEGER)
+                return token.value
+            case 'SQRT':
+                self.eat(SQRT)
+                self.eat(LP)
+                inner = self.expr()
+                self.eat(RP)
+                return inner ** 0.5
+            case 'LOG':
+                op = token.value
+                self.eat(LOG)
+                self.eat(LP)
+                inner = self.expr()
+                self.eat(RP)
+                if op == 'log2':
+                    return self._ln(inner) / self._ln(2)
+                elif op == 'log10':
+                    return self._ln(inner) / self._ln(10)
                 self.error()
-
-        if token.type == LP:
-            self.eat(LP)
-            result = self.expr()
-            self.eat(RP)
-            return result
-
-        self.error()
-
+            case 'LP':
+                self.eat(LP)
+                result = self.expr()
+                self.eat(RP)
+                return result
+            case _:
+                self.error()
+    #==================================================================#
+    #==================================================================#
     def term(self):
         result = self.factor()
 
@@ -231,6 +214,8 @@ class Interpreter(object):
                 result = result * self.factor()
             elif token.type == DIV:
                 self.eat(DIV)
+                if self.factor() == 0:
+                    raise ZeroDivisionError("division by zero")
                 result = result / self.factor()
             elif token.type == MOD:
                 self.eat(MOD)
@@ -238,9 +223,10 @@ class Interpreter(object):
             elif token.type == POW:
                 self.eat(POW)
                 result = result ** self.factor()
-
+                
         return result
-
+    #==================================================================#
+    #==================================================================#
     def comparison(self):
         left = self.term()
 
@@ -270,9 +256,10 @@ class Interpreter(object):
                 self.eat(NE)
                 right = self.term()
                 return TRUE if left != right else FALSE
-
+            
         return left
-
+    #==================================================================#
+    #==================================================================#
     def expr(self):
         # prime the first token on first call
         if self.current_token is None:
@@ -285,7 +272,7 @@ class Interpreter(object):
             if isinstance(result, str):
                 # already boolean TRUE/FALSE; disallow further arithmetic
                 self.error()
-                
+
             token = self.current_token
             if token.type == PLUS:
                 self.eat(PLUS)
@@ -293,5 +280,7 @@ class Interpreter(object):
             elif token.type == MINUS:
                 self.eat(MINUS)
                 result = result - self.comparison()
-
+                
         return result
+#======================================================================================#
+#======================================================================================#
